518ea2fcdcf62bebee491bb7cd527a425b0f07d5
Artem <garribond85@gmail.com>
2020-12-15 21:25:00 +0500

    Привел переменые balance, transfer к типу int64

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 func main() {
 	var balance int32 = 15_000_000_00
 	var transfer int32 = 10_000_000_00
-	total := balance + transfer
+	total := int64(balance) + int64(transfer)
 	println(total)
 }
