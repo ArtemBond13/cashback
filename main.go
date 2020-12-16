6e71bb40b7ec3d6a736e6627d32067a2d598e1fa
ArtemBond13 <75979537+ArtemBond13@users.noreply.github.com>
2020-12-16 10:50:46 +0500

    Привел  переменные к типу uint32, uint64

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-	var balance int32 = 15_000_000_00
-	var transfer int32 = 10_000_000_00
-	total := int64(balance) + int64(transfer)
+	var balance uint32 = 15_000_000_00
+	var transfer uint32 = 10_000_000_00
+	total := uint64(balance) + uint64(transfer)
 	println(total)
 }
